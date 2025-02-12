990cbdb12cefeeaaaaeca3ff3d63fa45bf854522
Vanessa Bizzell <vanessabizzell@gmail.com>
2025-02-12 16:10:02 +0000

    first draft of writing area submission functions

--- writingprompt/src/app/page.js
+++ writingprompt/src/app/page.js
@@ -1,14 +1,24 @@
 import HeroPrompt from "@/components/HeroPrompt";
 import WritingArea from "@/components/WritingArea.js";
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  
+  const [username] = useState(""); // state to hold username
+  const [writingPrompt, setWritingPrompt] = useState(""); // State to hold writingPrompt
+
+  const client = new ApiClient(); // Initialize your client
 
   return (
     <div>
-      <HeroPrompt  />
-      <WritingArea />
+      {/* Pass setWritingPrompt to HeroPrompt */}
+      <HeroPrompt setWritingPrompt={setWritingPrompt} />
+      
+      {/* Pass writingPrompt and username to WritingArea */}
+      <WritingArea
+        client={client}
+        username={username}
+        writingPrompt={writingPrompt}
+      />
     </div>
   );
 };
