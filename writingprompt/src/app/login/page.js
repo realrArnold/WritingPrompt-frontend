0320ccd819ab98245713a2c9b0ccdb49e3c12b0b
hali-27 <halima_k@hotmail.co.uk>
2025-02-20 16:16:07 +0000

    navbar added to login, signup and promptspage

--- writingprompt/src/app/login/page.js
+++ writingprompt/src/app/login/page.js
@@ -4,11 +4,13 @@ import React from 'react'
 import Login from '@/components/Login'
 import { ApiClient } from '../../../apiclient/client'
 // import { useRouter } from 'next/navigation'
+import Nav from "@/components/Nav";
 
 const page = () => {
   // const router = useRouter();
   return (
     <div> 
+      <Nav />
       <Login />
       
     </div>
--- writingprompt/src/components/Nav.js
+++ writingprompt/src/components/Nav.js
@@ -27,13 +27,13 @@ const Navigation = () => {
           <div className="fixed inset-0 bg-white bg-opacity-100 z-40 flex flex-col items-start shadow-md transition-all duration-300 p-4 w-64">
             <button onClick={toggleMenu} className="self-end mb-4"><X size={30} /></button>
             <ul className="w-full">
-              <li><button onClick={() => handleNavigation("/")} className="block py-2 px-4 hover:text-blue-500">Home</button></li>
+              <li><button onClick={() => handleNavigation("/promptspage")} className="block py-2 px-4 hover:text-blue-500">Home</button></li>
               <li><button onClick={() => handleNavigation("/userDashboard")} className="block py-2 px-4 hover:text-blue-500">Dashboard</button></li>
               <li><button onClick={() => handleNavigation("/promptspage")} className="block py-2 px-4 hover:text-blue-500">Prompts</button></li>
               <li><button onClick={() => handleNavigation("/noticeboard")} className="block py-2 px-4 hover:text-blue-500">Notice Board</button></li>
-              <li><button onClick={() => handleNavigation("/login")} className="block py-2 px-4 bg-blue-500 text-white rounded-lg">Login</button></li>
-              <li><button onClick={() => handleNavigation("/signup")} className="block py-2 px-4 bg-green-500 text-white rounded-lg">Sign Up</button></li>
-              <li><button onClick={() => handleNavigation("/logout")} className="block py-2 px-4 bg-red-500 text-white rounded-lg">Logout</button></li>
+              <li><button onClick={() => handleNavigation("/login")} className="block py-2 px-4 hover:text-blue-500">Login</button></li>
+              <li><button onClick={() => handleNavigation("/signup")} className="block py-2 px-4 hover:text-blue-500">Sign Up</button></li>
+              <li><button onClick={() => handleNavigation("/logout")} className="block py-2 px-4 hover:text-blue-500">Logout</button></li>
             </ul>
           </div>
         )}
