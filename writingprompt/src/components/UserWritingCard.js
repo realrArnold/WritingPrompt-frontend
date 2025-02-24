f11a76c0679ee4c576e72b2a02efcc7ecdc7cb0d
Vanessa Bizzell <vanessabizzell@gmail.com>
2025-02-24 13:04:14 +0000

    title input added. Layout changes

--- writingprompt/src/components/UserWritingCard.js
+++ writingprompt/src/components/UserWritingCard.js
@@ -16,27 +16,28 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Share2 } from "lucide-react"
+import { Share2 } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
-export default function UserWritingCard({  words, writingPrompt, genre, date }) {
+export default function UserWritingCard({ words, writingPrompt, genre, date }) {
   return (
-    <Card className="w-full">
+    <Card className="w-full flex flex-col h-full bg-gradient-to-bl from-zinc-50 to-zinc-200">
       <CardHeader>
         <CardTitle>{date}</CardTitle>
         <Label htmlFor="name">The prompt:</Label>
-        <CardDescription>"{writingPrompt}"</CardDescription>
+        <CardDescription className="italic">"{writingPrompt}"</CardDescription>
       </CardHeader>
-      <CardContent>
-        <form>
-          <div className="">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Your writing:</Label>
-              <p className="text-gray-600">{words}</p>
-            </div>
+      <CardContent className="flex-grow flex flex-col justify-between">
+        <form className="flex flex-col h-full">
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="name">Your writing:</Label>
+            <p className="text-gray-600">{words}</p>
+          </div>
+          <div id="inputs" className="flex flex-col space-y-3 pt-4 mt-auto">
             <div className="flex flex-col space-y-3 pt-4">
               <Label htmlFor="genre">Genre</Label>
-              <Select>
-                <SelectTrigger id="genre">
+              <Select className="">
+                <SelectTrigger id="genre" className="bg-white">
                   <SelectValue placeholder="Select" />
                 </SelectTrigger>
                 <SelectContent position="popper">
@@ -49,12 +50,16 @@ export default function UserWritingCard({  words, writingPrompt, genre, date }) 
                   <SelectItem value="who-cares">Whatever - I don't fit in a box!</SelectItem>
                 </SelectContent>
               </Select>
+              <div className="grid w-full max-w-sm items-center gap-1.5 pt-4">
+                <Label htmlFor="title">Would you like to add a title?</Label>
+                <Input id="title" type="string" className="bg-white" />
+              </div>
             </div>
           </div>
         </form>
       </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button ><Share2 /> Share</Button>
+      <CardFooter className="flex justify-between place-items-end">
+        <Button><Share2 /> Share</Button>
         <Button variant="destructive">Delete</Button>
       </CardFooter>
     </Card>
