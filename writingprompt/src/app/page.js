927a19a30a0810894826e0302ea845656adc8582
hali-27 <halima_k@hotmail.co.uk>
2025-02-27 12:15:13 +0000

    landing page is now the promptspage

--- writingprompt/src/app/page.js
+++ writingprompt/src/app/page.js
@@ -1,16 +1,44 @@
 
-import axios from "axios";
-import Signup from "@/components/Signup";
-import Nav from "@/components/Nav";
+// import axios from "axios";
+// import Signup from "@/components/Signup";
+// import Nav from "@/components/Nav";
 
-const Page = () => {
+// const Page = () => {
+//   return (
+//     <div>
+//       <Nav />
+//      <Signup />
+//     </div>
+//   );
+// };
+
+// export default Page;
+
+"use client"
+
+import React, { useState } from "react";
+import Nav from "@/components/Nav";
+import HeroPrompt from "@/components/HeroPrompt";
+import WritingArea from "@/components/WritingArea.js"; 
+import EntriesWrapper from "@/components/EntriesWrapper"
+import { ApiClient } from '../../apiclient/client';
+
+
+const Home = () => {
+  const [writingPrompt, setWritingPrompt] = useState("");
+
+  const client = new ApiClient(); 
+
   return (
     <div>
       <Nav />
-     <Signup />
+      <HeroPrompt setWritingPrompt={setWritingPrompt} />
+      <WritingArea client={client} writingPrompt={writingPrompt} />
+      <div>
+        <EntriesWrapper client={client} />
+      </div>
     </div>
   );
 };
 
-export default Page;
-
+export default Home;
